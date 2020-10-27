8694be666f8743812b518b47abf65738ac0653bc
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2020-10-27 15:30:10 +0300

    Updates

---
+++ precache-manifest.e22b6ec888c3d04a5fc0b50a0ef18aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3388747150897bdd5b5f0375018dfe89",
+    "url": "/kakoi-to-pirat.github.io/weather/index.html"
+  },
+  {
+    "revision": "1b08e785150339b9388e",
+    "url": "/kakoi-to-pirat.github.io/weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e8762d2ef33af7229c4",
+    "url": "/kakoi-to-pirat.github.io/weather/static/js/2.95b872a3.chunk.js"
+  },
+  {
+    "revision": "a93d10281e959eef3091bccd72c878f8",
+    "url": "/kakoi-to-pirat.github.io/weather/static/js/2.95b872a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b08e785150339b9388e",
+    "url": "/kakoi-to-pirat.github.io/weather/static/js/main.ae4d35bf.chunk.js"
+  },
+  {
+    "revision": "52fa068604e46ebd5d7c",
+    "url": "/kakoi-to-pirat.github.io/weather/static/js/runtime-main.fb1b1d52.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Horocek.github.io/weather/precache-manifest.4d683c13559ec482966b8176d7b15b7a.js"
+  "/kakoi-to-pirat.github.io/weather/precache-manifest.e22b6ec888c3d04a5fc0b50a0ef18aa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Horocek.github.io/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kakoi-to-pirat.github.io/weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
